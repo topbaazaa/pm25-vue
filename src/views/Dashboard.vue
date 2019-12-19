1df10118206533ebbe7a5258a7674d3e4bc6f8cd
theeramat sangsin <theeramat1123.ts@gmail.com>
2019-12-20 01:12:30 +0700

    add by P'met

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,240 +1,159 @@
 <template>
-  <div class="page-container">
-    <v-row>
-      <v-tabs centered slider-color="yellow" background-color="white" fixed-tabs color="yellow">
-        <v-tab>INSIDE</v-tab>
-        <v-tab>OUTSIDE</v-tab>
+    <div class="page-container">
+        <v-row>
+            <v-tabs centered slider-color="yellow" background-color="white" fixed-tabs color="yellow">
+                <v-tab>INSIDE</v-tab>
+                <v-tab>OUTSIDE</v-tab>
+                <v-tab-item v-for="n in 2" :key="n">
+                    <v-container fluid id="firstcontain">
+                        <v-card class="mx-auto" max-width="100%" color="#FFD600" id="vcardicleft">
+                            <br />
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <div v-if="humidity > 60">
+                                        <v-icon color="#FFFFFF" size="60px">{{ items[0] }}</v-icon>
+                                    </div>
+                                    <div v-else>
+                                        <v-icon color="#FFFFFF" size="60px">{{ items[1] }}</v-icon>
+                                    </div>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                        <v-card class="mx-auto" max-width="95%" color="#FFEB34" id="vcardicmid">
+                            <v-list-item three-line>
+                                <v-list-item-content>
+                                    <div class="overline mb-4">OVERLINE</div>
+                                    <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+                                    <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                        <v-card class="mx-auto" max-width="95%" color="#FFEB34" id="vcardicright">
+                            <v-list-item three-line>
+                                <v-list-item-content>
+                                    <div class="overline mb-4">OVERLINE</div>
+                                    <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+                                    <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                        <br />
 
-        <v-tab-item v-for="n in 2" :key="n" >
-          <v-container fluid id="firstcontain">
-            <v-card
-              class="mx-auto"
-              max-width="100%"
-              color="#FFD600"
-              id="vcardicleft" 
-            >
-            <br>
-             <v-list-item>
-              <v-list-item-content >
-              <v-icon
-              color="#FFFFFF"
-              size = "60px"
-              
-              >{{ items }}</v-icon>
-              </v-list-item-content>
-              </v-list-item>
-            </v-card>
-            <v-card
-              class="mx-auto"
-              max-width="95%"
-              color="#FFEB34"
-              id="vcardicmid"
-            >
-             <v-list-item three-line>
-              <v-list-item-content >
-              <div class="overline mb-4">OVERLINE</div>
-               <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-               <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-               </v-list-item-content>
-                 </v-list-item>
-            </v-card>
-             <v-card
-              class="mx-auto"
-              max-width="95%"
-              color="#FFEB34"
-              id="vcardicright"
-            >
-             <v-list-item three-line>
-              <v-list-item-content >
-              <div class="overline mb-4">OVERLINE</div>
-               <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-               <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-               </v-list-item-content>
-                 </v-list-item>
-                         </v-card>
-             <br>
+                        <v-card class="mx-auto" max-width="100%" color="#FFFFFF" id="vcardicleft2">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-row justify="center">
+                                        <img class="mr-3" :src="require('../thermometer (1).png')" height="60" />
+                                        <v-text class="font" color="#FFFFFF" size="60px">{{ temperature }}</v-text>
+                                    </v-row>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                        <v-card class="mx-auto" max-width="95%" color="#FFFFFF" id="vcardicmid2">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-row justify="center">
+                                        <img class="mr-3" :src="require('../rain-drops (1).png')" height="60" />
+                                        <v-text class="font" color="#FFFFFF" size="60px">{{ humidity }}</v-text>
+                                    </v-row>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                        <v-card class="mx-auto" max-width="95%" color="#FFFFFF" id="vcardicright2">
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-row justify="center">
+                                        <img class="mr-3" :src="require('../sun (1).png')" height="60" />
+                                    </v-row>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-card>
+                        <br />
+                        <br />
+                        <br />
+                        <v-row align="center" justify="center">
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
 
-              <v-card
-              class="mx-auto"
-              max-width="100%"
-              color="#FFFFFF"
-              id="vcardicleft2"
-              
-            >
-             <v-list-item >
-              <v-list-item-content >
-               <v-row justify ="center">
-              <img class="mr-3" :src="require('../thermometer (1).png')" height="60"  />
-               </v-row>
-               </v-list-item-content>
-                 </v-list-item>
-            </v-card>
-            <v-card
-              class="mx-auto"
-              max-width="95%"
-              color="#FFFFFF"
-              id="vcardicmid2"
-            >
-             <v-list-item>
-               <v-list-item-content >
-               <v-row justify ="center">
-                  <img class="mr-3" :src="require('../rain-drops (1).png')" height="60"  />
-                  <v-text
-                  class="font" 
-                  color="#FFFFFF"
-                  size = "60px">
-              
-              
-              >{{ value }}</v-icon></v-text>
-               </v-row>
-               </v-list-item-content>
-             </v-list-item>
-            </v-card>
-             <v-card
-              class="mx-auto"
-              max-width="95%"
-              color="#FFFFFF"
-              id="vcardicright2"
-            >
-             <v-list-item>
-               <v-list-item-content >
-               <v-row justify ="center">
-              <img class="mr-3" :src="require('../sun (1).png')" height="60"  />
-               </v-row>
-               </v-list-item-content>
-                 </v-list-item>
-                         </v-card>
-             <br>
-              <br>
-            <br>
-             <v-row align="center" justify="center">
-            <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
 
-          <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
-  
-     
-      <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
-  
-    
-      <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
-  
-          
-     </v-row>
-    <v-row align="center" justify="center">
-     
-          <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
-  
-          
- 
-    
-   
-  
-      <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
-      <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >  
-            Show Overlay
-          </v-btn>
-  
-    
-      <v-btn
-            color="success"
-            class="ma-2"
-            @click="overlay = !overlay"
-            height="38px"
-          >
-            Show Overlay
-          </v-btn>
-  
-         
-     </v-row>
-     
-          </v-container>
-        </v-tab-item>
-      </v-tabs>
-    </v-row>
-    <v-overlay
-            :absolute="absolute"
-            :value="overlay"
-          >
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
+
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
+                        </v-row>
+                        <v-row align="center" justify="center">
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
+
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
+
+                            <v-btn color="success" class="ma-2" @click="overlay = !overlay" height="38px">Show Overlay</v-btn>
+                        </v-row>
+                    </v-container>
+                </v-tab-item>
+            </v-tabs>
+        </v-row>
+        <v-overlay :absolute="absolute" :value="overlay">
             <v-card
-            @click="overlay = false"
-            color="#FFD600"
-            height="40vw"
-            width="29vw"
-            class="overlayblock"
-            id="overlayleft">
-            </v-card>
+                @click="overlay = false"
+                color="#FFD600"
+                height="40vw"
+                width="29vw"
+                class="overlayblock"
+                id="overlayleft"
+            ></v-card>
             <v-card
-            @click="overlay = false"
-            color="#FFFFFF"
-            height="40vw"
-            width="32vw"
-            class="overlayblock"
-            id="overlayright">
-            </v-card>
-          </v-overlay>
-  </div>
-  
+                @click="overlay = false"
+                color="#FFFFFF"
+                height="40vw"
+                width="32vw"
+                class="overlayblock"
+                id="overlayright"
+            ></v-card>
+        </v-overlay>
+    </div>
 </template>
 <script>
+import * as firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyCKoH1orPrpHKW9X3273TKUAHeLELmb15E',
+  authDomain: 'dust-meter.firebaseapp.com',
+  databaseURL: 'https://dust-meter.firebaseio.com',
+  projectId: 'dust-meter',
+  storageBucket: 'dust-meter.appspot.com',
+  messagingSenderId: '2281533914',
+  appId: '1:2281533914:web:19c722a94bf6b5a07b90e3',
+  measurementId: 'G-PZYC9L456L'
+};
+firebase.initializeApp(config)
+var database = firebase.database()
+var contactRef = database.ref('/logDHT')
 export default {
-  data() {
+  data () {
     return {}
   },
   methods: {},
+  mounted () {
+    contactRef.limitToLast(1).on('value', (snapshot) => {
+      var data = snapshot.val()
+      for (var i in data) {
+        this.humidity = (data[i].humidity)
+        this.temperature = (data[i].temperature)
+        if (this.humidity > 60) {
+          document.getElementById('vcardicleft').style.backgroundColor = 'red'
+        }
+        else {document.getElementById('vcardicleft').style.backgroundColor = 'green'}
+      }
+    })
+  },
   data: () => ({
     absolute: true,
+    humidity: [],
+    temperature: [],
     overlay: false,
-    items: 'mdi-weather-hail', 
-    
+    items: ['mdi-weather-hail','home'],
+
   })
- 
+
 }
 </script>
 
@@ -243,72 +162,69 @@ export default {
   height: 100%;
   *background-color: rgb(255, 255, 255);
 }
-.mystyle{
- background-color:"#16a085"
+.mystyle {
+  background-color: '#16a085';
 }
-.mx-auto{
+.mx-auto {
   display: inline-block;
 }
-.font{
-  font-family: "Lucida Sans Unicode";
+.font {
+  font-family: 'Lucida Sans Unicode';
   /*margin:initial;*/
   margin-top: auto;
   margin-bottom: auto;
 }
-.overlayblock{
+.overlayblock {
   display: block;
   float: left;
 }
-#overlayleft
-{
-border-radius: 20px 0 0 20px;
+#overlayleft {
+  border-radius: 20px 0 0 20px;
 }
-#overlayright
-{
+#overlayright {
   border-radius: 0 20px 20px 0;
 }
-#vcardicleft{
+#vcardicleft {
   width: 32%;
   height: 130px;
   border-radius: 2vw 0vw 0vw 2vw;
   float: left;
 }
-#vcardicmid{
+#vcardicmid {
   width: 23%;
   height: 130px;
   border-radius: 0 0 0 0;
 }
-#vcardicright{
+#vcardicright {
   width: 45%;
   height: 130px;
   border-radius: 0vw 2vw 2vw 0vw;
   clear: right;
 }
-#vcardicleft2{
+#vcardicleft2 {
   width: 33%;
   height: 80px;
   margin-top: 10px;
   border-radius: 2vw 0vw 0vw 2vw;
   float: left;
 }
-#vcardicmid2{
+#vcardicmid2 {
   width: 34%;
   height: 80px;
   margin-top: 10px;
   border-radius: 0 0 0 0;
 }
-#vcardicright2{
+#vcardicright2 {
   width: 33%;
   height: 80px;
   margin-top: 10px;
   border-radius: 0vw 2vw 2vw 0vw;
   clear: right;
 }
-#firstcontain{
+#firstcontain {
   margin-right: auto;
   margin-left: auto;
   display: block;
   width: 90%;
 }
-
 </style>
--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -1,66 +1,47 @@
 <template>
-  <div slot="media">
-    <v-tabs
-      centered
-      slider-color="yellow"
-      background-color="white"
-      fixed-tabs
-      color="yellow"
-      id="graphnav"
-    >
-      <v-tab>DUST</v-tab>
-      <v-tab>TEMP</v-tab>
-      <v-tab>HUMIDITY</v-tab>
-      <v-tab>LUMINANCE</v-tab>
-      <!-- --------------------------------- #Grapg 1----------------------------------------------- -->
-      <v-tab-item>
-        <v-container fluid>
-          <v-card hight="100%" width="100%" outlined>
-            <vue-highcharts :highcharts="Highcharts" :options="options[0]" ref="drilldownChart"></vue-highcharts>
-          </v-card>
-        </v-container>
-      </v-tab-item>
-      <!-- --------------------------------- #Grapg 2----------------------------------------------- -->
-      <v-tab-item>
-        <v-container fluid>
-          <v-card hight="100%" width="100%" outlined>
-            <vue-highcharts :highcharts="Highcharts" :options="options[1]" ref="drilldownChart"></vue-highcharts>
-          </v-card>
-        </v-container>
-      </v-tab-item>
-      <!-- --------------------------------- #Grapg 3----------------------------------------------- -->
-      <v-tab-item>
-        <v-container fluid>
-          <v-card hight="100%" width="100%" outlined>
-            <vue-highcharts :highcharts="Highcharts" :options="options[2]" ref="drilldownChart"></vue-highcharts>
-          </v-card>
-        </v-container>
-      </v-tab-item>
-      <!-- --------------------------------- #Grapg 4----------------------------------------------- -->
-      <v-tab-item>
-        <v-container fluid>
-          <v-card hight="100%" width="100%" outlined>
-            <vue-highcharts :highcharts="Highcharts" :options="options[3]" ref="drilldownChart"></vue-highcharts>
-          </v-card>
-        </v-container>
-      </v-tab-item>
-    </v-tabs>
-  </div>
+    <div slot="media">
+        <v-tabs centered slider-color="yellow" background-color="white" fixed-tabs color="yellow" id="graphnav">
+            <v-tab>DUST</v-tab>
+            <v-tab>TEMP</v-tab>
+            <v-tab>HUMIDITY</v-tab>
+            <v-tab>LUMINANCE</v-tab>
+            <!-- --------------------------------- #Grapg 1----------------------------------------------- -->
+            <v-tab-item v-for="n in 4" :key="n" @click="test(n)">
+                <v-container fluid>
+                    <v-card hight="100%" width="100%" outlined>
+                        <vue-highcharts :highcharts="Highcharts" :options="options[n]" ref="drilldownChart"></vue-highcharts>
+                    </v-card>
+                </v-container>
+            </v-tab-item>
+        </v-tabs>
+    </div>
 </template>
 <script>
 import VueHighcharts from 'vue2-highcharts'
 import Highcharts from 'highcharts'
+// console.log(contactRef)
 export default {
   components: {
     // Drilldown,
     VueHighcharts
   },
-  data() {
+  mounted () {
+    contactRef.limitToLast(1).on('value', (snapshot) => {
+      var data = snapshot.val()
+      for (var i in data) {
+        this.humidity.push(data[i].humidity)
+      }
+      console.log(this.humidity)
+    })
+  },
+  data () {
     return {
+      contacts: {},
+      humidity: [],
       name: ['DUST', 'TEMP', 'HUMIDITY', 'LUMINANCE'],
       Highcharts: Highcharts,
-      options: [
-        {
+      options:
+        ['', {
           chart: {
             height: '100%',
 
@@ -68,13 +49,15 @@ export default {
             animation: Highcharts.svg, // don't animate in old IE
             marginRight: 10,
             events: {
-              load: function() {
+              load: function () {
                 // set up the updating of the chart each second
+                var me = this
+                console.log(me)
                 var series = this.series[0]
-                setInterval(function() {
+                setInterval(function () {
                   var x = new Date().getTime(), // current time
                     y = Math.random() * 10 + 1
-                  series.addPoint([x, y], true, true)
+
                 }, 1000)
               }
             }
@@ -115,94 +98,29 @@ export default {
             {
               name: 'Random data',
               color: '#FFEB34',
-              data: (function() {
-                // generate an array of random data
-                var data = [],
-                  time = new Date().getTime(),
-                  i
-                for (i = -19; i <= 0; i += 1) {
-                  data.push({
-                    x: time + i * 1000,
-                    y: Math.random() * 10 + 1
-                  })
-                }
-                return data
-              })()
+              data: this.humidity
+              //   data: (function () {
+              //     // generate an array of random data
+              //     var data = [],
+              //       time = new Date().getTime(),
+              //       i
+              //     for (i = -19; i <= 0; i += 1) {
+              //       data.push({
+              //         x: time + i * 1000,
+              //         y: Math.random() * 10 + 1
+              //       })
+              //     }
+              //     return data
+              //   })()
             }
           ]
-        },
-        {
-          chart: {
-            height: '100%',
+        }]
 
-            type: 'spline',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-              load: function() {
-                // set up the updating of the chart each second
-                var series = this.series[0]
-                setInterval(function() {
-                  var x = new Date().getTime(), // current time
-                    y = Math.random() * 10 + 1
-                  series.addPoint([x, y], true, true)
-                }, 1000)
-              }
-            }
-          },
-          time: {
-            useUTC: false
-          },
-          title: {
-            text: 'TEMP'
-          },
-          xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150
-          },
-          yAxis: {
-            title: {
-              text: null
-            },
-            plotLines: [
-              {
-                value: 0,
-                width: 1,
-                color: '#808080'
-              }
-            ]
-          },
-          tooltip: {
-            headerFormat: '<b>{series.name}</b><br/>',
-            pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-          },
-          legend: {
-            enabled: false
-          },
-          exporting: {
-            enabled: false
-          },
-          series: [
-            {
-              name: 'Random data',
-              color: '#FFEB34',
-              data: (function() {
-                // generate an array of random data
-                var data = [],
-                  time = new Date().getTime(),
-                  i
-                for (i = -19; i <= 0; i += 1) {
-                  data.push({
-                    x: time + i * 1000,
-                    y: Math.random() * 10 + 1
-                  })
-                }
-                return data
-              })()
-            }
-          ]
-        }
-      ]
+    }
+  },
+  methods: {
+    test (n) {
+      console.log(n)
     }
   }
 }
